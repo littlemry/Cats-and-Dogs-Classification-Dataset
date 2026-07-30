f4cc59cb67d0eba397cd6f7814e59185422f04c0
littlemry <you@example.com>
2026-07-30 10:34:38 +0530

    Update

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import tensorflow as tf
 import numpy as np
 
-model = tf.keras.models.load_model("cat_dog_model.keras")
+MODEL_PATH = "cat_dog_model.keras"
+IMAGE_PATH = "OIP.jpeg"
 
-img = tf.keras.preprocessing.image.load_img(
-    "OIP.jpeg",
-    target_size=(128,128)
-)
+model = tf.keras.models.load_model(MODEL_PATH)
 
-img = tf.keras.preprocessing.image.img_to_array(img)
-img = np.expand_dims(img, axis=0)
-img = img / 255.0
 
-prediction = model.predict(img)
+def predict_image(image_path: str = IMAGE_PATH) -> str:
+    img = tf.keras.preprocessing.image.load_img(
+        image_path,
+        target_size=(128, 128),
+    )
 
-if prediction[0][0] > 0.5:
-    print("Dog")
-else:
-    print("Cat")
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+
+    prediction = model.predict(img, verbose=0)
+
+    return "Dog" if prediction[0][0] > 0.5 else "Cat"
+
+
+def app():
+    return predict_image()
+
+
+if __name__ == "__main__":
+    print(predict_image())
